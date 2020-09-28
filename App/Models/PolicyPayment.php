5b475c37eb49e4778536467614310627309285ed
Rafael <Bytheway0708@gmail.com>
2020-09-28 19:18:42 -0400

    Added ID to report

--- App/Models/PolicyPayment.php
+++ App/Models/PolicyPayment.php
@@ -4,7 +4,7 @@ namespace App\Models;
 class PolicyPayment extends \ActiveRecord\Model{
 	static $belongs_to=[['account'],['client'],['user']];
 	public function serialize(){
-		$payment=$this->to_array(['only'=>['amount','currency','policy_status']]);
+		$payment=$this->to_array(['only'=>['amount','currency','policy_status','id']]);
 		$payment['date']=$this->created_at->format('d-m-Y');
 		$payment['account']=$this->account->name;
 		$payment['client']=$this->client->first_name;
