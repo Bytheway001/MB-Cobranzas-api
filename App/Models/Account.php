8f7eea3606f17602b0e23aabbf291dd869ad61ef
Rafael <Bytheway0708@gmail.com>
2021-02-01 14:23:22 -0400

    Fixed Saldos Iniciales

--- App/Models/Account.php
+++ App/Models/Account.php
@@ -56,17 +56,18 @@ class Account extends \ActiveRecord\Model {
     public function getSaldoAt($date) {
         $date = new \DateTime($date);
         $date = $date->format('Y-m-d');
-
         $fechaInicial = $this->last_balance_date->format('Y-m-d');
         $saldo = ['USD'=>$this->last_balance_usd, 'BOB'=>$this->last_balance_bob];
 
         if ($date > $fechaInicial) {
-            $query = "SELECT * from movimiento_de_cuenta where account_id = $this->id AND DATE(date) between '$fechaInicial' and '$date'";
+            $query = "SELECT * from movimiento_de_cuenta where account_id = $this->id AND DATE(date) > '$fechaInicial' and DATE(date) < '$date'";
             $movements = $this->find_by_sql($query);
             foreach ($movements as $movement) {
                 $saldo[$movement->currency] = $saldo[$movement->currency] + $movement->debe - $movement->haber;
             }
-        } else {
+        } 
+
+        else {
             $query = "SELECT * from movimiento_de_cuenta where account_id = $this->id AND DATE(date) between '$date' and '$fechaInicial'";
             $movements = $this->find_by_sql($query);
             foreach ($movements as $movement) {
